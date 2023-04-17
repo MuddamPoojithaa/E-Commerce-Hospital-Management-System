40386c64edd7df5a262a7f2391dfcdab5621797b
MuddamPoojitha <poojithamuddam172@gmail.com>
2023-04-17 21:21:39 +0530

    Hospital commit

---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Patient Models
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pid = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=10)
+    age = models.CharField(max_length=3)
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    bloodgroup = models.CharField(max_length=10)
+    casepaper = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.user.first_name}'
+
+
+# Doctor Models            
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    did = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=10)
+    age = models.CharField(max_length=3)
+    gender = models.CharField(max_length=10)
+    Department = models.CharField(max_length=20)
+    attendance = models.CharField(max_length=10)
+    salary = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.user.first_name}'
